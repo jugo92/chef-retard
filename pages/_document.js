6db2c1c7917e283e87a3c00e2d626d28914b6b7c
jugo92 <jugzek92@outlook.fr>
2023-11-11 11:17:32 +0100

    add title

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
+        <title>Chef Retard</title>
         <meta
           name="description"
           content="Des excuses hilarantes pour justifier un retard au travail. Rendez votre journée plus légère avec nos idées loufoques."
